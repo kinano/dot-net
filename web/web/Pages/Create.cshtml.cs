2a36ecd742d72de9d0a418a9c1bae4857748d791
Kinan Faham <k@theSilentCamera.com>
2018-12-22 16:51:58 -0500

    Play around with the basic console app and basic MVC web app

---
+++ web/web/Pages/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace web.Pages
+{
+    public class CreateModel : PageModel
+    {
+        // Make the DB context private and readonly to protect it from other modules
+        private readonly AppDbContext _db;
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+        [TempData]
+        public string Message { get; set; }
+
+        private ILogger<CreateModel> Log;
+
+        public CreateModel(AppDbContext db, ILogger<CreateModel> log)
+        {
+            _db = db;
+            Log = log;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Customers.Add(Customer);
+            await _db.SaveChangesAsync();
+            var msg = $"Customer {Customer.Name} added!";
+            Message = msg;
+            Log.LogCritical(msg);
+            return RedirectToPage("/Index");
+        }
+    }
+}
---
+++ web/web/Pages/Edit.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace web.Pages
+{
+
+    public class EditModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+        public EditModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Customer = await _db.Customers.FindAsync(id);
+            if (Customer == null)
+            {
+                return RedirectToPage("/Index");
+            }
+            return Page();
+        }
+
+        public async Task<ActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _db.Attach(Customer).State = EntityState.Modified;
+            try
+            {
+                await _db.SaveChangesAsync();
+
+            }
+            catch (DbUpdateException e)
+            {
+                throw new Exception($"Customer {Customer.Id} not found", e);
+            }
+            return Redirect("/Index");
+        }
+
+    }
+}
